50aa4e8eea437076df30abf188b85d37551c7ba0
Harshit Jain <iharshitjain99@gmail.com>
2025-07-19 11:47:22 +0530

    fixed cors issue

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,11 @@ connectDB();
 
 // Middlewares
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET','POST','PUT','DELETE'],
+  credentials: true
+}));
 
 // API Endpoints
 app.get("/", (req, res) => {
